54414edcfddaa683f1f558fc511d2e5231df9cb3
hasithasandun <hasiofficial2002@gmail.com>
2024-09-10 12:18:23 +0530

    data base updated

---
+++ src/app/api/add/route.ts
@@ -0,0 +1,37 @@
+import { NextResponse, NextRequest } from "next/server";
+import mysql from 'mysql2/promise';
+import { GetDBSettings } from "@/sharedCode/common";
+
+let connectionParams = GetDBSettings();
+
+export async function POST(request: NextRequest) {
+    try {
+        // Establish a connection to the database
+        const connection = await mysql.createConnection(connectionParams);
+
+        // Parse the JSON body from the request
+        const body = await request.json();
+
+        // Extract data from the body
+        const { email,password } = body;
+
+        // Insert the data into your database
+        const [result] = await connection.execute(
+            'INSERT INTO uom.users (email,password) VALUES (?, ?)', 
+            [email,password]
+        );
+
+        // Close the database connection
+        await connection.end();
+
+        // Return a success response
+        return NextResponse.json({ message: "Data inserted successfully", result });
+    
+    } catch (err) {
+        console.log('ERROR: API - ', err as Error);
+
+        const response = { error: (err as Error).message };
+        
+        return NextResponse.json(response, { status: 500 });
+    }
+}
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,8 @@
+import { Registation } from '@/components/Registation'
+import React from 'react'
+
+export default function page() {
+  return (
+    <Registation/>
+  )
+}
