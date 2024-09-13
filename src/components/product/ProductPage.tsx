b36f94fa92d561e4bf6bf8388a0629ae3002b63f
hasithasandun <hasiofficial2002@gmail.com>
2024-09-13 19:03:16 +0530

    viewcart

--- src/components/product/ProductPage.tsx
+++ src/components/product/ProductPage.tsx
@@ -43,22 +43,22 @@ const handleDecrement=()=>{
       };
     return (
 
-<div className="py-8 bg-gray-100 dark:bg-gray-800">
+<div className="py-8 bg-gray-100 dark:bg-gray-100">
     <div className="max-w-6xl px-4 mx-auto sm:px-6 lg:px-8">
         <div className="flex flex-col -mx-4 md:flex-row">
             <div className="px-4 md:flex-1">
-                <div className="h-[460px] rounded-lg bg-gray-300 dark:bg-gray-700 mb-4">
+                <div className="h-[460px] rounded-lg bg-gray-300 dark:bg-gray-300 mb-4">
                     <img className="object-cover w-full h-full" src={product.image} alt="Product Image" />
                 </div>
                 <div className="flex mb-4 -mx-2">
                     {isInCart?
                        <div className="w-full px-2">
-                       <button onClick={handleAddToCart} className="w-full px-4 py-2 font-bold text-white bg-gray-900 rounded-full dark:bg-gray-600 hover:bg-gray-800 dark:hover:bg-gray-700">Add to Cart</button>
+                       <button onClick={handleAddToCart} className="w-full px-4 py-2 font-bold text-white bg-gray-900 rounded-full hover:bg-gray-800 dark:hover:bg-gray-700">Add to Cart</button>
                    </div>: 
                    
                    
                    <div className="w-full px-2">
-                       <button onClick={()=>router.push("/cart")} className="w-full px-4 py-2 font-bold text-white bg-gray-900 rounded-full dark:bg-gray-600 hover:bg-gray-800 dark:hover:bg-gray-700">View Cart</button>
+                       <button onClick={()=>router.push("/cart")} className="w-full px-4 py-2 font-bold text-white bg-red-600 rounded-full hover:bg-gray-800 dark:hover:bg-gray-700">View Cart</button>
                    </div>}
                  
             
@@ -66,23 +66,23 @@ const handleDecrement=()=>{
                 </div>
             </div>
             <div className="px-4 md:flex-1">
-                <h2 className="mb-2 text-2xl font-bold text-gray-800 dark:text-white">{product.name}</h2>
-                <p className="mb-4 text-sm text-gray-600 dark:text-gray-300">
+                <h2 className="mb-2 text-2xl font-bold text-gray-800 ">{product.name}</h2>
+                <p className="mb-4 text-sm text-gray-600 ">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sed
                     ante justo. Integer euismod libero id mauris malesuada tincidunt.
                 </p>
                 <div className="flex mb-4">
                     <div className="mr-4">
-                        <span className="font-bold text-gray-700 dark:text-gray-300">Price:</span>
-                        <span className="text-gray-600 dark:text-gray-300">${product.price}</span>
+                        <span className="font-bold text-gray-700 ">Price:</span>
+                        <span className="text-gray-600 ">${product.price}</span>
                     </div>
                     <div>
-                        <span className="font-bold text-gray-700 dark:text-gray-300">Availability:</span>
-                        <span className="text-gray-600 dark:text-gray-300">In Stock</span>
+                        <span className="font-bold text-gray-700 ">Availability:</span>
+                        <span className="text-gray-600 ">In Stock</span>
                     </div>
                 </div>
                 <div className="mb-4">
-                    <span className="font-bold text-gray-700 dark:text-gray-300">Select Color:</span>
+                    <span className="font-bold text-gray-700 ">Select Color:</span>
                     <div className="flex items-center mt-2">
                         <button className="w-6 h-6 mr-2 bg-gray-800 rounded-full dark:bg-gray-200"></button>
                         <button className="w-6 h-6 mr-2 bg-red-500 rounded-full dark:bg-red-700"></button>
@@ -90,8 +90,8 @@ const handleDecrement=()=>{
                         <button className="w-6 h-6 mr-2 bg-yellow-500 rounded-full dark:bg-yellow-700"></button>
                     </div>
                 </div>
-                <div className="mb-4">
-                    <span className="font-bold text-gray-700 dark:text-gray-300">Select Size:</span>
+                {/* <div className="mb-4">
+                    <span className="font-bold text-gray-700 ">Select Size:</span>
                     <div className="flex items-center mt-2">
                         <button className="px-4 py-2 mr-2 font-bold text-gray-700 bg-gray-300 rounded-full dark:bg-gray-700 dark:text-white hover:bg-gray-400 dark:hover:bg-gray-600">S</button>
                         <button className="px-4 py-2 mr-2 font-bold text-gray-700 bg-gray-300 rounded-full dark:bg-gray-700 dark:text-white hover:bg-gray-400 dark:hover:bg-gray-600">M</button>
@@ -99,11 +99,11 @@ const handleDecrement=()=>{
                         <button className="px-4 py-2 mr-2 font-bold text-gray-700 bg-gray-300 rounded-full dark:bg-gray-700 dark:text-white hover:bg-gray-400 dark:hover:bg-gray-600">XL</button>
                         <button className="px-4 py-2 mr-2 font-bold text-gray-700 bg-gray-300 rounded-full dark:bg-gray-700 dark:text-white hover:bg-gray-400 dark:hover:bg-gray-600">XXL</button>
                     </div>
-                </div>
+                </div> */}
 
 
                 <div className="mb-4">
-                    <span className="font-bold text-gray-700 dark:text-gray-300">Quantity:</span>
+                    <span className="font-bold text-gray-700 ">Quantity:</span>
                     <div className="flex items-center mt-2">
                         <button onClick={()=>handleDecrement()} className="px-4 py-2 mr-2 font-bold text-gray-700 bg-gray-300 rounded-full dark:bg-gray-700 dark:text-white hover:bg-gray-400 dark:hover:bg-gray-600">-</button>
 
@@ -114,8 +114,8 @@ const handleDecrement=()=>{
                     </div>
                 </div>
                 <div>
-                    <span className="font-bold text-gray-700 dark:text-gray-300">Product Description:</span>
-                    <p className="mt-2 text-sm text-gray-600 dark:text-gray-300">
+                    <span className="font-bold text-gray-900 ">Product Description:</span>
+                    <p className="mt-2 text-sm text-gray-900 ">
                      {product.description}
                     </p>
                 </div>
