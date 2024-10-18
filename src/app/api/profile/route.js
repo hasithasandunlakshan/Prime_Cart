d334386345459d43b75009021395880001c8511f
Raleesa Perera <raleesasvperera@gmail.com>
2024-10-18 16:52:58 +0530

    add route.js for api/profile

---
+++ src/app/api/profile/route.js
@@ -0,0 +1,36 @@
+
+import { NextResponse } from "next/server";
+import mysql from 'mysql2/promise';
+import {GetDBSettings} from "../../../sharedCode/common"
+// export const GetDBSettings = () => {
+//   return {
+//     host: 'mysql-13eae395-hasiofficial2002-f9b9.c.aivencloud.com',
+//     user: 'avnadmin',
+//     password: 'AVNS_TVFlXjqaBIrs_ewIgdP', 
+//     database: 'defaultdb',
+//     port: 18475,
+//   };
+// };
+
+let connectionparams = GetDBSettings();
+
+export async function GET(request) {
+    let connection;
+
+    try {
+        connection = await mysql.createConnection(connectionparams);
+        //const query = 'select * from defaultdb.UserAddress left outer join defaultdb.RegisteredUser on RegisteredUser.userId=UserAddress.userId';
+        const query = 'SELECT * FROM defaultdb.UserAddress WHERE userId=1';
+        const [result]=await connection.execute(query);
+
+        return NextResponse.json(result);
+    } catch (error) {
+        console.error('Database error:', error); 
+        return NextResponse.json({ error: 'Failed to fetch user credentials' }, { status: 500 });
+    } finally {
+        if (connection) {
+            await connection.end(); 
+            
+        }
+    }
+}
