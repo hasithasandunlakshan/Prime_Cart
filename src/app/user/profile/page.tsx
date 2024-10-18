f8513d1d214353fa80fee033fe37a31d4784e29a
hasithasandun <hasiofficial2002@gmail.com>
2024-10-19 00:48:32 +0530

    update profile

--- src/app/user/profile/page.tsx
+++ src/app/user/profile/page.tsx
@@ -1,10 +1,8 @@
-"use client"
+
 import React, { useEffect, useState } from 'react'
 import Profile from '@/components/profile/profile'
 import Payment from '@/components/Payment/payment'
 const page = () =>{
-  const [user, setUser] = useState([]);
-  const [error, setError] = useState(null);
 
 
   return (
