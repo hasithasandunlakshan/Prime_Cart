4718faaa64bfc6c0fa6aa7519b88f32d6ad819ff
Navatharshini.B <107050309+navatharshini@users.noreply.github.com>
2024-10-25 15:27:20 +0530

    Add files via upload

---
+++ src/app/daily_offer/page.tsx
@@ -0,0 +1,11 @@
+import Dailyoffer from '@/components/daily_offer/daily_offer'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+     <Dailyoffer/>
+     
+    </div>
+  )
+}
