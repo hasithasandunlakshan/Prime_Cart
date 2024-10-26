4c8b4e3099ff0b2e81cce4942c27c6abc4f0fe0f
Raleesa Perera <raleesasvperera@gmail.com>
2024-10-26 22:14:53 +0530

    Update user\profile

--- src/app/user/profile/page.tsx
+++ src/app/user/profile/page.tsx
@@ -2,17 +2,25 @@
 import React, { useEffect, useState } from 'react'
 import Profile from '@/components/profile/Profile'
 import Payment from '@/components/Payment/payment'
+import UpdateProfile from '@/components/profile/updateProfile/updateProfile'
 const page = () =>{
 
 
   return (
     
       <div>
-        <Profile/>
- 
-  
-    </div>
+        <div>
+        <Profile/> 
+        </div>
+        
+        <div>
+        <UpdateProfile/>
+        </div>
+      </div>  
    
   )
+      
+
+      
 }
 export default page;
